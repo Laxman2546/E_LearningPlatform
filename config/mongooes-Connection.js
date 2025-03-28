67ac434fdb2217b75bf5a46080e71afb7b55fa94
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-03-28 20:12:18 +0530

    changes in the register

--- config/mongooes-Connection.js
+++ config/mongooes-Connection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 mongoose
   .connect(process.env.MONGODB_URI, {
-    serverSelectionTimeoutMS: 5000, // Add this to avoid long timeouts
+    serverSelectionTimeoutMS: 5000,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
